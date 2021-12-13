35d86f17a91bd9d898a0945b77d82f4f2da41f2b
Frédéric Jacques <gary.bob88@gmail.com>
2021-12-12 22:24:27 -0500

    format .net 6

--- BossRC3Editor/BossRC3.cs
+++ BossRC3Editor/BossRC3.cs
@@ -1,61 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BossRC3Editor;
 
-namespace BossRC3Editor
+public class BossRC3
 {
-    public class BossRC3
+    private readonly DirectoryInfo _roland;
+    private readonly DirectoryInfo _wave;
+    private readonly DirectoryInfo _data;
+
+    public BossRC3(DirectoryInfo directory)
     {
-        private readonly DirectoryInfo _roland;
-        private readonly DirectoryInfo _wave;
-        private readonly DirectoryInfo _data;
+        _roland = directory.GetDirectories("ROLAND").First();
+        _wave = _roland.GetDirectories("WAVE").First();
+        _data = _roland.GetDirectories("DATA").First();
+    }
 
-        public BossRC3(DirectoryInfo directory)
+    public DirectoryInfo Loop(string name)
+    {
+        return _wave.GetDirectories(name).First();
+    }
+
+    public void CopyLoop(string source, string destination)
+    {
+        var loop = Loop(source);
+        var dest = Loop(destination);
+
+        dest.GetFiles().ToList().ForEach(f => f.Delete());
+
+        var files = loop.GetFiles();
+
+        files.Single();
+
+        for (int i = 0; i < files.Length; i++)
         {
-            _roland = directory.GetDirectories("ROLAND").First();
-            _wave = _roland.GetDirectories("WAVE").First();
-            _data = _roland.GetDirectories("DATA").First();
+            files[i].CopyTo($"{dest.FullName}\\{destination}.WAV");
         }
+    }
 
-        public DirectoryInfo Loop(string name)
+    public double GetTempo(string v)
+    {
+        var lines = File.ReadAllLines(_data.GetFiles("MEMORY.RC3").Single().FullName);
+
+        for (int i = 0; i < lines.Length; i++)
         {
-            return _wave.GetDirectories(name).First();
-        }
+            Console.WriteLine(lines[i]);
 
-        public void CopyLoop(string source, string destination)
-        {
-            var loop = Loop(source);
-            var dest = Loop(destination);
-
-            dest.GetFiles().ToList().ForEach(f => f.Delete());
-
-            var files = loop.GetFiles();
-
-            files.Single();
-
-            for (int i = 0; i < files.Length; i++)
-            {
-                files[i].CopyTo($"{dest.FullName}\\{destination}.WAV");
-            }
-        }
-
-        public double GetTempo(string v)
-        {
-            var lines = File.ReadAllLines(_data.GetFiles("MEMORY.RC3").Single().FullName);
-
-            for (int i = 0; i < lines.Length; i++)
+            if (lines[i].Contains(v))
             {
                 Console.WriteLine(lines[i]);
-
-                if (lines[i].Contains(v))
-                {
-                    Console.WriteLine(lines[i]);
-                }
             }
-
-            return 0.0;
         }
+
+        return 0.0;
     }
 }
